18ff4775f1df8f51af3b66a1453461a3b1f1d390
awol2010ex <awol2010ex@gmail.com>
2012-09-18 16:45:41 +0800

    树状列出cognos 8列表

--- WebContent/views/main.jsp
+++ WebContent/views/main.jsp
@@ -13,23 +13,82 @@
 <jsp:include  page="/css.jsp"  flush="true" />
 
 <script type='text/javascript'>
+var tree = null; ;//报表目录树
+
 $(function (){
+	
+	        //划分布局
             $("#layout1").ligerLayout({
-                minLeftWidth:80,
+                minLeftWidth:100,
+                leftWidth: 300,
                 minRightWidth:80
             });
+	        
+            var height = $(".l-layout-center").height();
+	        
+          //面板
+            $("#accordion1").ligerAccordion({ height: height - 24, speed: null });
+
+	        
+	        
+           //报表目录树
+            tree = $("#tree1")
+			.ligerTree(
+					{
+
+						url : '<%=contextPath%>/restful/cognos8/path/list/',
+						checkbox: false,
+						nodeWidth : 200,
+						textFieldName : 'name',
+	                    slide: false,
+						idFieldName:"id",
+						isLeaf:function(data){
+							//这是BUG吧,为什么isLeaf 是 true ,hasChildren 就是true呢,看源代码
+							return true;
+						},
+						onBeforeExpand : function(node) {
+
+							//防止重复加载,使用isloaded 标记
+                            if($("#"+node.data.id).data("isloaded")){
+                               return; 
+                            }
+							var params = {};
+							if (node.data.searchPath) {
+								params.searchPath = node.data.searchPath+"/folder";//展开节点的searchPath,只显示目录
+							}
+							
+							tree
+									.loadData(
+											$("#"+node.data.id)[0],
+											'<%=contextPath%>/restful/cognos8/path/list/',
+											params);
+							$("#"+node.data.id).data("isloaded",true)
+						}
+
+					});
+
+
 });
 
 </script>
 </head>
-<body>
+<body >
 
- <div id="layout1">
-             <div  position="left"></div>
-            <div position="center" title="报表列表">
-            </div>
-            <div position="top"><%=cognos8_str%></div>
-        </div> 
+	<div id="layout1" style="overflow:auto">
+		<div position="left" title="主要菜单" id="accordion1">
+		
+		      <div title="报表目录" class="l-scroll">
+                         <ul id="tree1" style="margin-top:3px;">
+              </div>
+
+		</div>
+		<div position="center" title="报表列表">
+
+			
+
+		</div>
+		<div position="top"><%=cognos8_str%></div>
+	</div>
 
 </body>
 </html>
--- src/com/cmweb/cognos8/web/Cognos8Controller.java
+++ src/com/cmweb/cognos8/web/Cognos8Controller.java
@@ -1,9 +1,10 @@
 package com.cmweb.cognos8.web;
 
-import java.io.UnsupportedEncodingException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
@@ -16,56 +17,99 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.cmweb.cognos8.CRNConnect;
 import com.cmweb.cognos8.service.ICognos8Service;
 import com.cognos.developer.schemas.bibus._3.BaseClass;
+import com.cognos.developer.schemas.bibus._3.Folder;
+import com.cognos.developer.schemas.bibus._3.Report;
+import com.cognos.developer.schemas.bibus._3.ReportView;
+
 //cognos 8相关 restful 服务类
 @Controller
 @RequestMapping(value = "/cognos8")
 public class Cognos8Controller {
 	private final static Logger logger = LoggerFactory
-	.getLogger(Cognos8Controller.class);
-	
+			.getLogger(Cognos8Controller.class);
+
 	@Autowired
-	ICognos8Service ICognos8Service;//目录操作
-	//子节点列表
+	ICognos8Service ICognos8Service;// 目录操作
+
+	// 子节点列表
 	@RequestMapping(value = "/path/list")
-	public  void  getChildren(HttpServletRequest request,	HttpServletResponse response) {
-		String searchPath =request.getParameter("searchPath");//搜索路径
-		
-		if(searchPath!=null ){
-			try {
-				searchPath =new String(searchPath.getBytes("ISO-8859-1"),"GBK");
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				logger.error("",e);
-			}
-		}
-		/*  第一层 searchPath =/content/*
-		 *  第二层 searchPath =/content/folder[@name='总经理组']/*
-		 * 
-		 * */
+	public void getChildren(HttpServletRequest request,
+			HttpServletResponse response) {
+		String searchPath = request.getParameter("searchPath");// 搜索路径
+
+		/*
+		 * 第一层 searchPath =/content/* 第二层 searchPath
+		 * =/content/folder[@name='总经理组']/*
+		 */
 		try {
-			BaseClass[]   queryList =ICognos8Service.getChildren(this.getConnect(), searchPath);
-			
-			if(queryList!= null  && queryList.length>0){
-				for(BaseClass c :queryList){
-					logger.error("",c);
+
+			JSONArray result = new JSONArray();// 返回结果
+			if (searchPath != null) {// 非根节点
+				BaseClass[] queryList = ICognos8Service.getChildren(
+						this.getConnect(), searchPath);
+
+				if (queryList != null && queryList.length > 0) {
+
+					for (BaseClass c : queryList) {
+						result.add(new JSONObject()
+								.element("searchPath",
+										c.getSearchPath().getValue())
+								// searchPath
+								.element("name", c.getDefaultName().getValue())
+								// 节点名称
+								.element("isexpand", false)
+								// 不自动展开
+								.element("icon", this.getIconURL(request, c))
+								// 图标
+								.element("children", new JSONArray())
+								.element("id",
+										c.getStoreID().getValue().getValue()));// ID
+					}
+
 				}
+			} else {// 根节点
+				result.add(new JSONObject().element("searchPath", "/content")
+						// searchPath
+						.element("name", "公共文件夹")
+						// 节点名称
+						.element("isexpand", false)
+						// 不自动展开
+						.element("children", new JSONArray())
+						.element(
+								"icon",
+								request.getContextPath()
+										+ "/static/images/icon_folder.gif")// 图标
+						.element("id", "-1"));
 			}
-			
+
+			response.getWriter().print(result.toString());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			logger.error("",e);
+			logger.error("", e);
 		}
-		
+
 	}
-	
-	
-	//取得Cognos8连接
-	public CRNConnect getConnect(){
+
+	// 取得节点ICON
+	public String getIconURL(HttpServletRequest request, BaseClass c) {
+		if (c instanceof ReportView) {
+			return request.getContextPath() + "/static/images/icon_package.gif";
+		} else if (c instanceof Report) {// 报表
+			return request.getContextPath() + "/static/images/icon_report.gif";
+		} else if (c instanceof Folder) {// 目录
+			return request.getContextPath() + "/static/images/icon_folder.gif";
+		}
+		return null;
+	}
+
+	// 取得Cognos8连接
+	public CRNConnect getConnect() {
 		Subject currentUser = SecurityUtils.getSubject();
-		
-		CRNConnect  connection=(CRNConnect)currentUser.getSession().getAttribute("connection");
-		
+
+		CRNConnect connection = (CRNConnect) currentUser.getSession()
+				.getAttribute("connection");
+
 		return connection;
-		
+
 	}
 }
